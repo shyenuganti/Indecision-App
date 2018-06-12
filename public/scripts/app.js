29e8968bd1049561a0deefc28acf3b912a513b6f
shyenuganti <shyenuganti@gmail.com>
2018-06-11 23:01:13 -0400

    Running Babel
    Added a src  folder.
    Added .babelrc file for babel presets
    Added a txt file with the list of commands described in the lectures for quick reference

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,12 @@
+'use strict';
+
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-// var template = <p>Hello, World!</p>;
 var template = React.createElement(
-  "h1",
- null,
-  "Hello, World!"
+    'h1',
+    null,
+    'Hello, World !'
 );
 var appRoot = document.getElementById('app');
 
---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = (
+    <h1>Hello, World !</h1>
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
