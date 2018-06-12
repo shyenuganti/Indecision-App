ae84ca7547691ea6618e81ceb94e9de9ff6ea20f
shyenuganti <shyenuganti@gmail.com>
2018-06-11 23:32:21 -0400

    Dynamic strings in JSX
    Here we show examples of how to use user defined dynamic strings inside JSX snippets.
    
    Plese take a note of the auto-generated app.js inside scripts folder to see how our JSX is converted into React method calls.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,67 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-    'h1',
+    'div',
     null,
-    'Hello, World !'
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var user = {
+    name: 'Sri Harsha',
+    age: 26,
+    location: 'Columbus'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Name: ',
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+// Only the last render is effective. 
+// That overrides the previous onseeked. 
+// Be careful
+// ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,8 +2,35 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = (
-    <h1>Hello, World !</h1>
+   <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item two</li>
+    </ol>
+   </div>
+);
+
+var user = {
+    name:'Sri Harsha',
+    age:26,
+    location:'Columbus'
+};
+
+var templateTwo=(
+    <div>
+        <h1>Name: {user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
 );
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+// Only the last render is effective. 
+// That overrides the previous onseeked. 
+// Be careful
+// ReactDOM.render(templateTwo, appRoot);
+
